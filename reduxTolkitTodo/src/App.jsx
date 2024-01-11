6464308c42348f56e32b7428ce22a051e034a0a1
Dharmendra kumar ram <136229876+dharmendraram@users.noreply.github.com>
2024-01-11 22:44:47 +0545

    complete learn with redux tolkit

---
+++ reduxTolkitTodo/src/App.jsx
@@ -0,0 +1,15 @@
+
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+function App() {
+
+  return (
+    <div className='text-center'>
+     <h1>Learn Redux TolKit</h1>
+     <AddTodo/>
+     <Todos/>
+    </div>
+  )
+}
+
+export default App
